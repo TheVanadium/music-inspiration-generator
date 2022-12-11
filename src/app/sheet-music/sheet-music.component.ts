f40cdde0fb1d68034b079604db18e1ba48a9cdae
Garret <garretthecastro@gmail.com>
2022-12-11 14:44:39 -0800

    remove copy-pasted comments sheet-music component

--- src/app/sheet-music/sheet-music.component.ts
+++ src/app/sheet-music/sheet-music.component.ts
@@ -11,24 +11,30 @@ export class SheetMusicComponent {
   constructor() {}
 
   ngOnInit() {
-    const { Renderer, Stave } = Vex.Flow;
+    const { Formatter, Renderer, Stave, StaveNote, Voice } = Vex.Flow;
 
-    // Create an SVG renderer and attach it to the DIV element
     const sheetMusic : HTMLElement = document.getElementById("sheet-music")!;
     const div : HTMLElement = sheetMusic!; 
     const renderer = new Renderer(div, Renderer.Backends.SVG);
 
-    // Configure the rendering context.
     renderer.resize(500, 500);
     const context = renderer.getContext();
 
-    // Create a stave of width 400 at position 10, 40 on the canvas.
     const stave = new Stave(10, 40, 400);
-
-    // Add a clef and time signature.
     stave.addClef("treble");
-
-    // Connect it to the rendering context and draw!
     stave.setContext(context).draw();
+
+    const notes = [
+      new StaveNote({ keys: ["c/4"], duration: "q" }),
+      new StaveNote({ keys: ["d/4"], duration: "q" }),
+      new StaveNote({ keys: ["b/4"], duration: "qr" }),
+    ];
+
+    const voice = new Voice({ num_beats: 3, beat_value: 4 });
+    voice.addTickables(notes);
+
+    new Formatter().joinVoices([voice]).format([voice], 350);
+
+    voice.draw(context, stave);
   }
 }
