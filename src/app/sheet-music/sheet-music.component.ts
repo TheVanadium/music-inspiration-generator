e2f51c4be535dd65999657280a5052152833c471
Garret <garretthecastro@gmail.com>
2022-12-11 09:10:03 -0800

    get vexflow to render properly

--- src/app/sheet-music/sheet-music.component.ts
+++ src/app/sheet-music/sheet-music.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import * as Vex from 'vexflow';
 
 @Component({
   selector: 'app-sheet-music',
@@ -7,4 +8,28 @@ import { Component } from '@angular/core';
 })
 export class SheetMusicComponent {
 
+  constructor() {}
+
+  ngOnInit() {
+    const { Factory } = Vex.Flow;
+
+        // Create a VexFlow renderer attached to the DIV element with id="sheet-music".
+        const vf = new Factory({ renderer: { elementId: 'sheet-music', width: 300, height: 300} });
+        const score = vf.EasyScore();
+        const system = vf.System();
+
+        // Create a 4/4 treble stave and add two parallel voices.
+        system.addStave({
+        voices: [
+            // Top voice has 4 quarter notes with stems up.
+            score.voice(score.notes('C5/q, B4, A4, G#4', { stem: 'up' })),
+        
+            // Bottom voice has two half notes, with stems down.
+            score.voice(score.notes('C#4/h, C#4', { stem: 'down' }))
+        ]
+        }).addClef('treble').addTimeSignature('4/4');
+
+        // Draw it!
+        vf.draw();
+  }
 }
